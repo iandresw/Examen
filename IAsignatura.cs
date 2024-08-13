5cd1e5e2bd8fd965244e76c46ed5005a518e7abf
iandresw <rbejarano@unah.hn>
2024-08-12 21:29:38 -0600

    Create IAsignatura.cs
    se creo la interface asiganatura y se crearon 4 métodos

---
+++ IAsignatura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal interface IAsignatura
+    {
+        public double CalcularNotaFinal();
+        public double CalcularNotaFinal(int Nota1, int Nota2, int Nota3);
+        public string MensajeNotaFinal(double notaFninal);
+        public void Imprimir();
+    }
+}
